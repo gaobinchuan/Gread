4ab8e97974115d129cd041c8a1660300f619c575
gaobinchuan <gaobinchuan@sina.com>
2016-03-11 21:14:34 +0800

    Fix bugs

--- gread.sh
+++ gread.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if ! [ -f ~/.gread/${1}.mp3 ]; then
+if ! [ -f ${HOME}/.gread/${1}.mp3 ]; then
 
-    if ! [ -d ~/.gread ]; then
-        mkdir ~/.gread
+    if ! [ -d ${HOME}/.gread ]; then
+        mkdir ${HOME}/.gread
     fi
 
     l416=`curl -s http://www.iciba.com/${1} | grep '<span>美'`
     u1=${l416#*displayAudio(\'}
     u2=${u1%%\')\"*}
-    cd ~/.gread
-    wget -q -P "~/.gread" -O "${1}.mp3" "$u2"
+    cd ${HOME}/.gread
+    wget -q -P "${HOME}/.gread" -O "${1}.mp3" "$u2"
 
 fi
 
-mplayer "~/.gread/${1}.mp3" >/dev/null 2>/dev/null &
+mplayer "${HOME}/.gread/${1}.mp3" 1>/dev/null 2>/dev/null &
 
